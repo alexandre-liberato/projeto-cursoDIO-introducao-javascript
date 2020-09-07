6cdbff9e09f7c2e7c662b144dff0102188fde5c6
alexandre-liberato <alexandre_liberato@hotmail.com>
2020-09-06 21:01:02 -0300

    Arquivo com 17 códigos em JavaScript sendo Array e Dicionario os principais

---
+++ js/arrays-dicionario.js
@@ -0,0 +1,103 @@
+// LEIA AS INSTRUÇÕES DE USO ABAIXO PARA ENTENDER COMO UTILIZAR
+//
+// É necessário fazer alterações no código para ver o seu funcionamento no arquivo "index.html". Para isso:
+//
+// 1) Escolha qual código deseja ver funcionando (Código 1, Código 2, Código 3, Código 4, etc)
+//
+// 2) Verifique se o cóðigo escolhido inicia com /*. Se não tiver, significa que esse código está ativo
+// e é esse código que está funcionando no arquivo "index.html"
+//
+// 3) Para que um código inativo funcione, é necessário colocar /* antes da palavra "Código" e */ após a
+// última linha do código ATIVO e apagar o /* e o */ do código INATIVO.
+
+// Código 1: Esse algoritmo emite um popup na tela do usuário com a mensagem MEU PRIMEIRO JAVA SCRPIT
+alert("Meu primeito Java Script");
+
+/* Código 2: Esse algoritmo emite um popup na tela do usuário com a mensagem BEM VINDO ALEXANDRE LIBERATO
+var nome = "Alexandre Liberato";
+alert("Bem vindo " + nome);
+*/
+
+/* Código 3: Este algoritmo emite um popup na tela do usuário com a mensagem ALEXANDRE LIBERATO TEM 41 ANOS
+var nome = "Alexandre Liberato";
+var idade = "41";
+alert(nome + " tem " + idade + " anos.");
+*/
+
+/* Código 4: Este algoritmo emite um popup na tela do usuário com a soma dos dois números (valor1 + valor2). É Possível fazer as quatro operações matemáticas
+var valor1 = 10;
+var valor2 = 20;
+alert(valor1 + valor2);
+*/
+
+/* Código 5: Este algoritmo emite um popup na tela do usuário com a concatenação de duas strings (valor1 + valor2)
+var valor1 = "10";
+var valor2 = "20";
+alert(valor1 + valor2);
+*/
+
+/* Código 6: Este algoritmo emite um popup na tela do usuário com a substituição de uma informação da frase
+var frase = "Japão tem o melhor time de futebol do mundo!";
+alert(frase.replace("Japão", "Brasil"));
+*/
+
+/* Código 7: Este algoritmo emite um popup na tela do usuário colocando todas as letras da frase em maiúscula
+var frase = "Brasil tem o melhor time de futebol do mundo!";
+alert(frase.toUpperCase());
+*/
+
+/* Código 8: Este algoritmo emite um popup na tela do usuário colocando todas as letras da frase em minúscula
+var frase = "Brasil tem o melhor time de futebol do mundo!";
+alert(frase.toLowerCase());
+*/
+
+/* Código 9: Este algoritmo emite um popup na tela do usuário com o resultado da lista abaixo
+var lista = ["Maça", "Pêra", "Uva", "Limão"];
+alert(lista[2]);
+*/
+
+/* Código 10: Este algoritmo emite um popup na tela do usuário com o resultado da adição de um item que não estava na lista
+var lista = ["Maça", "Pêra", "Uva", "Limão"];
+lista.push("Laranja");
+alert(lista);
+*/
+
+/* Código 11: Este algoritmo emite um popup na tela do usuário com o resultado da remoção de um item que não estava na lista
+var lista = ["Maça", "Pêra", "Uva", "Limão"];
+lista.pop();
+alert(lista);
+*/
+
+/* Código 12: Este algoritmo emite um popup na tela do usuário com o resultado da quantidade de itens contidos em uma lista
+var lista = ["Maça", "Pêra", "Uva", "Limão"];
+alert(lista.length);
+*/
+
+/* Código 13: Este algoritmo emite um popup na tela do usuário com o resultado inverso de itens contidos em uma lista
+var lista = ["Maça", "Pêra", "Uva", "Limão"];
+alert(lista.reverse());
+*/
+
+/* Código 14: Este algoritmo emite um popup na tela do usuário com o resultado dos itens contidos em uma lista
+var lista = ["Maça", "Pêra", "Uva", "Limão"];
+alert(lista.toString());
+// alert(lista[0]);              // Este alerta irá mostrar apenas o item selecionado na lista
+// alert(lista.toString()[0]);   // Este alerta irá mostrar apenas a primeira letra do item selecionado na lista
+*/
+
+/* Código 15: Este algoritmo emite um popup na tela do usuário com o resultado dos itens contidos em uma lista
+var lista = ["Maça", "Pêra", "Uva", "Limão"];
+alert(lista.join(" - "));
+*/
+
+/* Código 16: Este algoritmo emite um popup na tela do usuário com o resultado do item contidos em um dicionário
+var fruta = {nome: "Maça", cor: "Vermelha"};
+alert(fruta.nome)
+alert(fruta.cor)
+*/
+
+/* Código 17: Este algoritmo emite um popup na tela do usuário com o resultado dos itens em um dicionário
+var frutas = [{nome: "Maça", cor: "Vermelha"}, {nome: "Pêra", cor: "Amarela"}, {nome: "Uva", cor: "Roxa"}, {nome: "Limão", cor: "Verde"}];
+alert(frutas[0].nome)
+alert(frutas[2].cor)
+*/
